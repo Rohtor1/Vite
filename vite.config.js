7cc8f9e7132c5ab0a7ec9be155cde4723302988f
Sakari Santapukki <sakari.santapukki@outlook.com>
2024-10-29 13:36:12 +0200

    config

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Vite/', // Replace "your-repo-name" with the actual repository name
+  // You can add other configurations here if needed
+});
